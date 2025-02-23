80abe38446d536af48020839b5082bb5805f32bd
Derik1408 <85242612+gamerderik@users.noreply.github.com>
2025-02-23 18:45:29 +0300

    Update OwlHub.lua

--- Script Hubs/OwlHub.lua
+++ Script Hubs/OwlHub.lua
@@ -1,15 +1,1 @@
--- Yes the legendary owl hub
-
-local old
-old = hookfunction(game.HttpGetAsync, function(inst, url, state)
-  url = url:gsub('CriShoux', 'SiLeNSwOrD')
-  return old(inst, url, state)
-end)
-
-local old2
-old2 = hookfunction(game.HttpGet, function(inst, url, state)
-  url = url:gsub('CriShoux', 'SiLeNSwOrD')
-  return old2(inst, url, state)
-end)
-
-loadstring(game:HttpGet("https://github.com/SiLeNSwOrD/OwlHub/raw/master/OwlHub.txt"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/CriShoux/OwlHub/master/OwlHub.txt"))()
